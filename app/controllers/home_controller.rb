3c144b244c487d103226b7c81cae45dc84b715b3
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-03-17 18:41:11 -0700

    Yahoo Finance super basic integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,39 @@
-class HomeController < ApplicationController
-  def index
+class HomeController < ApplicationController  
+  def index   
+    
+    #!/usr/bin/ruby
+
+    @output = "foo";
+
+    
+    
+    
+    #base_uri = "http://ws.eoddata.com/data.asmx/"
+    
+  	#@response1 = HTTParty.get(base_uri + "Login",
+  	#                          :query => { :Username => "joeollis",
+  	#                                      :Password => "windowsce"})
+  	#@login_token = @response1["LOGINRESPONSE"]["Token"]
+
+  	#@response2 = HTTParty.get(base_uri + "SymbolHistoryPeriodByDateRange",
+  	#                          :query => { :Token => @login_token,
+  	#                                      :Exchange => "NASDAQ",
+  	#                                      :Symbol => "MSFT",
+  	#                                      :StartDate => "20090316",
+  	#                                      :EndDate => "20120301",
+  	#                                      :Period => "m"})
+
+#    require "open-uri"
+
+#    url = "http://www.google.com/finance/historical?q=NYSE:SMH"
+#    url_data = open(url).read()
+  	
+  	#require "CSV"
+
+  	#@arr_of_arrs = CSV.read("test1.csv")
+    #@arr_length = @arr_of_arrs.length
+  	
+
+  	
   end
 end
