7ba016388723c3d85b4af84482b2575117edb914
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-03-21 04:16:51 -0700

    refactored code to be more generic and created UI to select symbols

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,39 +1,7 @@
 class HomeController < ApplicationController  
-  def index   
-    
-    #!/usr/bin/ruby
-
-    @output = "foo";
-
-    
-    
-    
-    #base_uri = "http://ws.eoddata.com/data.asmx/"
-    
-  	#@response1 = HTTParty.get(base_uri + "Login",
-  	#                          :query => { :Username => "joeollis",
-  	#                                      :Password => "windowsce"})
-  	#@login_token = @response1["LOGINRESPONSE"]["Token"]
-
-  	#@response2 = HTTParty.get(base_uri + "SymbolHistoryPeriodByDateRange",
-  	#                          :query => { :Token => @login_token,
-  	#                                      :Exchange => "NASDAQ",
-  	#                                      :Symbol => "MSFT",
-  	#                                      :StartDate => "20090316",
-  	#                                      :EndDate => "20120301",
-  	#                                      :Period => "m"})
-
-#    require "open-uri"
-
-#    url = "http://www.google.com/finance/historical?q=NYSE:SMH"
-#    url_data = open(url).read()
-  	
-  	#require "CSV"
-
-  	#@arr_of_arrs = CSV.read("test1.csv")
-    #@arr_length = @arr_of_arrs.length
-  	
-
-  	
+  def index
+    @all_symbols = [ 'SPY', 'EFA', 'IEF', 'FXF', 
+      'IWM', 'EEM', 'TLT', 'IYM', 'IYR', 'GLD' ]
+    @default_symbols = [ 'SPY', 'EFA', 'IEF', 'FXF' ]    
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,68 @@
 module HomeHelper
+  def get_weekly(symbol)
+  	count = 0;
+  	quotes = Array.new
+
+  	YahooFinance::get_historical_quotes(symbol, 
+  	                                    Date.parse('2006-10-20'), 
+  	                                    Date.today()) do |row|
+  		if count == 1
+  			quotes.push Float(row[4])
+  			count += 1
+			elsif count == 4
+			  count = 0
+  		else
+  			count += 1
+  		end
+  	end
+
+    quotes.reverse
+  end
+  
+  def get_equal_weighting_returns(symbols)
+    all_quotes = Array.new
+    
+    # Get historical quotes for each symbol
+    symbols.each do |symbol|
+    	quotes = get_weekly(symbol)
+    	all_quotes.push quotes
+    end
+
+    # Safety check to ensure each symbol has same number of quotes
+    all_quotes.each do |quotes|
+    	if quotes.count != all_quotes[0].count
+    		raise 'Error: all of the symbols must have same number of quotes.' 
+    	end
+    end
+
+    # Loop through each all quotes and append combined return to output array
+    # starting with $100.00
+    i = 1
+    j = 0
+    returns = Array.new
+    returns.push 100.0
+
+    while i < all_quotes[0].count
+    	total_returns = Array.new
+    	percent_returns = Array.new
+    	weighted_returns = Array.new
+    	combined_return = 0.0
+
+    	while j < all_quotes.count
+    		total_returns.push all_quotes[j][i] - all_quotes[j][i - 1]
+    		percent_returns.push total_returns[j] / all_quotes[j][i - 1]
+    		j += 1
+    	end
+      
+      weighted_returns = percent_returns.map {|x| x / all_quotes.count}
+      combined_return = weighted_returns.inject {|sum, x| sum + x}
+
+    	returns.push (1 + combined_return) * returns[i - 1]
+
+    	i += 1
+    	j = 0
+    end
+    
+    returns
+  end
 end
