c0d3d646ab8b136ff38344db51d0a866611ba1d1
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-03-21 11:51:16 -0700

    added verbose output for date, close, and return

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,12 +2,14 @@ module HomeHelper
   def get_weekly(symbol)
   	count = 0;
   	quotes = Array.new
+  	quote_dates = Array.new
 
   	YahooFinance::get_historical_quotes(symbol, 
   	                                    Date.parse('2006-10-20'), 
   	                                    Date.today()) do |row|
-  		if count == 1
+  		if count == 2
   			quotes.push Float(row[4])
+  			quote_dates.push row[0]
   			count += 1
 			elsif count == 4
 			  count = 0
@@ -16,24 +18,32 @@ module HomeHelper
   		end
   	end
 
-    quotes.reverse
+    [quote_dates.reverse, quotes.reverse]
   end
   
   def get_equal_weighting_returns(symbols)
-    all_quotes = Array.new
+    @all_quotes = Array.new
+    @all_quote_dates = Array.new
     
     # Get historical quotes for each symbol
     symbols.each do |symbol|
-    	quotes = get_weekly(symbol)
-    	all_quotes.push quotes
+    	@all_quotes.push get_weekly(symbol)[1]
+    	@all_quote_dates.push get_weekly(symbol)[0]
     end
 
     # Safety check to ensure each symbol has same number of quotes
-    all_quotes.each do |quotes|
-    	if quotes.count != all_quotes[0].count
+    @all_quotes.each do |quotes|
+    	if quotes.count != @all_quotes[0].count
     		raise 'Error: all of the symbols must have same number of quotes.' 
     	end
     end
+    
+    # Safety check to ensure each symbol has same dates for quotes
+    @all_quote_dates.each do |quote_dates|
+      if quote_dates != @all_quote_dates[0]
+    		raise 'Error: all of the symbols must have same quotes on the same dates.' 
+      end
+    end    
 
     # Loop through each all quotes and append combined return to output array
     # starting with $100.00
@@ -42,19 +52,19 @@ module HomeHelper
     returns = Array.new
     returns.push 100.0
 
-    while i < all_quotes[0].count
+    while i < @all_quotes[0].count
     	total_returns = Array.new
     	percent_returns = Array.new
     	weighted_returns = Array.new
     	combined_return = 0.0
 
-    	while j < all_quotes.count
-    		total_returns.push all_quotes[j][i] - all_quotes[j][i - 1]
-    		percent_returns.push total_returns[j] / all_quotes[j][i - 1]
+    	while j < @all_quotes.count
+    		total_returns.push @all_quotes[j][i] - @all_quotes[j][i - 1]
+    		percent_returns.push total_returns[j] / @all_quotes[j][i - 1]
     		j += 1
     	end
       
-      weighted_returns = percent_returns.map {|x| x / all_quotes.count}
+      weighted_returns = percent_returns.map {|x| x / @all_quotes.count}
       combined_return = weighted_returns.inject {|sum, x| sum + x}
 
     	returns.push (1 + combined_return) * returns[i - 1]
