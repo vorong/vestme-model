78de7a3355c7b15a67f8ffe58159a6d7d04ec11e
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-03-21 10:25:45 -0700

    changed FXF to TIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController  
   def index
-    @all_symbols = [ 'SPY', 'EFA', 'IEF', 'FXF', 
+    @all_symbols = [ 'SPY', 'EFA', 'IEF', 'TIP', 
       'IWM', 'EEM', 'TLT', 'IYM', 'IYR', 'GLD' ]
-    @default_symbols = [ 'SPY', 'EFA', 'IEF', 'FXF' ]    
+    @default_symbols = [ 'SPY', 'EFA', 'IEF', 'TIP' ]    
   end
 end
