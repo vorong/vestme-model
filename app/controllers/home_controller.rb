c94e0b594d1c849fe0bcc795b6ed12d654d4896c
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-03-21 22:12:04 -0700

    added FXF back as optional checkbox

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController  
   def index
     @all_symbols = [ 'SPY', 'EFA', 'IEF', 'TIP', 
-      'IWM', 'EEM', 'TLT', 'IYM', 'IYR', 'GLD' ]
+      'IWM', 'EEM', 'TLT', 'IYM', 'IYR', 'GLD', 'FXF' ]
     @default_symbols = [ 'SPY', 'EFA', 'IEF', 'TIP' ]    
   end
 end
